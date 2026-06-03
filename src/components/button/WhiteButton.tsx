d445b1007361916ad8e22df253fb1732e4ec8c2f
fvlekk <fvlekk@gmail.com>
2026-06-04 01:04:51 +0200

    chore: suppression 86 screenshots, 15 composants/CSS morts, fusion règles CSS dupliquées howItWorks

--- src/components/button/WhiteButton.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { StyledWhiteButton } from "../../styles/button/styledWhiteButton";
-import type { ButtonProps } from "@/types/button/button";
-
-export default function WhiteButton({ title, type }: ButtonProps) {
-  return <StyledWhiteButton type={type}>{title}</StyledWhiteButton>;
-}
